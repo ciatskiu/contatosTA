a58489fe78b3207958eabac5f4b218add6bbf787
Carlos Eduardo Andrade Iatskiu <carlosiatskiu@MacBook-Pro-de-Carlos.local>
2023-11-30 10:15:12 -0300

    storage final

--- src/app/view/detalhar/detalhar.page.ts
+++ src/app/view/detalhar/detalhar.page.ts
@@ -14,6 +14,7 @@ export class DetalharPage implements OnInit {
   contato: Contato;
   indice: number;
   edicao: boolean = true;
+  public imagem: any;
 
   constructor(private router: Router,
     private firebase: FirebaseService) { }
@@ -32,9 +33,18 @@ export class DetalharPage implements OnInit {
     }
   }
 
+  uploadFile(imagem: any){
+    this.imagem = imagem.files;
+  }
+
   editar(){
     let novo: Contato = new Contato(this.nome, this.telefone);
-    this.firebase.update(novo, this.contato.id);
+    novo.id = this.contato.id;
+    if(this.imagem){
+      this.firebase.uploadImage(this.imagem, novo);
+    }else{
+      this.firebase.update(novo, this.contato.id);
+    }
     this.router.navigate(["/home"]);
   }
 
